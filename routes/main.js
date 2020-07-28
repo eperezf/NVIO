317f17a0597821ba697d032225739bf9d7a07b3f
Eduardo Pérez <eduardojoseperezf@gmail.com>
2020-07-28 00:25:15 -0400

    Base Quienes Somos

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,15 @@ router.get('/', (req, res) => {
   console.log("Index requested");
   var date = new Date();
   var year = date.getFullYear();
-  res.render('index', {year: year, title: "NVIO"});
+  res.render('index', {year: year, title: "NVIO | Inicio"});
+});
+
+//Quienes somos route
+router.get('/quienes-somos', (req, res) => {
+  console.log("Quienes Somos requested");
+  var date = new Date();
+  var year = date.getFullYear();
+  res.render('quienes-somos', {title: "NVIO | Quienes Somos"});
 });
 
 module.exports = router;
