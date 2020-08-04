e154ac845b58bb1e46f13fd012d9817587f158db
Eduardo Pérez <eduardojoseperezf@gmail.com>
2020-08-04 01:43:04 -0400

    Dashboard auth middleware
    Dashboard routes now redirect to login if user is not authenticated.

--- routes/dash.js
+++ routes/dash.js
@@ -3,56 +3,56 @@ const passport = require('passport');
 const router = express.Router();
 
 // Dashboard Index
-router.get('/', (req, res) => {
+router.get('/', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Dashboard";
     console.log("Dashboard Index Requested");
     res.render('dashboard/dashboard', {title: name});
 });
 
 // Dashboard Profile
-router.get('/perfil', (req, res) => {
+router.get('/perfil', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Mi Perfil";
     console.log("Dashboard Profile Requested");
     res.render('dashboard/dash-perfil', {title: name})
 });
 
 // Dashboard Make New Order
-router.get('/nuevo-envio', (req, res) => {
+router.get('/nuevo-envio', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Nuevo Envio";
     console.log("Dashboard New Order Requested");
     res.render('dashboard/dash-envio', {title: name});
 });
 
 // Dashboard Order History
-router.get('/hist-pedidos', (req, res) => {
+router.get('/hist-pedidos', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Historial Pedidos";
     console.log("Dashboard Order History Requested");
     res.render('dashboard/dash-hist-pedidos', {title: name});
 });
 
 // Dashboard Payment History
-router.get('/hist-pagos', (req, res) => {
+router.get('/hist-pagos', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Historial Pagos";
     console.log("Dashboard Payment History Requested");
     res.render('dashboard/dash-hist-pagos', {title: name});
 });
 
 // Dashboard Cost Tables
-router.get('/costos', (req, res) => {
+router.get('/costos', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Tablas de Costos";
     console.log("Dashboard Cost Tables Requested");
     res.render('dashboard/dash-costos', {title: name});
 });
 
 // Dashboard Support
-router.get('/soporte', (req, res) => {
+router.get('/soporte', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Ayuda y Soporte";
     console.log("Dashboard Support Requested");
     res.render('dashboard/dash-soporte', {title: name});
 });
 
 // Dashboard Edit Profile
-router.get('/editar-perfil', (req, res) => {
+router.get('/editar-perfil', passport.authenticate('jwt', {session: false, failureRedirect: '/login'}), (req, res) => {
     const name = "Editar Perfil";
     console.log("Dashboard Edit Profile Requested");
     res.render('dashboard/dash-editar-perfil', {title: name})
